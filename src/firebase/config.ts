f38ad3f34f53cca832ec5c23931b03294938e7d0
Bernardo De la garza <berniedlgg@gmail.com>
2025-11-11 20:40:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3430901982-a8cfe",
+  "appId": "1:1066262509761:web:a10da122563eb05cdebc40",
+  "apiKey": "AIzaSyB6-tJTbIMnvFe1erck1qajDoC5-11Avso",
+  "authDomain": "studio-3430901982-a8cfe.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1066262509761"
+};
